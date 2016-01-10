29121873ba37a86e6bdc9aa298fbcf654f9ca834
greenforest <foopop@hanmail.net>
2016-01-11 00:17:17 +0900

    Merge branch 'EndlessScrolling_Request'

--- Certis/app/src/main/java/com/certis/NoticeActivity.java
+++ Certis/app/src/main/java/com/certis/NoticeActivity.java
@@ -4,9 +4,11 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +19,7 @@ import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.certis.notice.Notice;
 import com.certis.notice.NoticeListAdapter;
+import com.certis.scroll.EndlessScrollListener;
 import com.certis.sqlite.DBHelper;
 import com.certis.volley.AppController;
 import com.github.clans.fab.FloatingActionButton;
@@ -41,11 +44,16 @@ public class NoticeActivity extends NavigationDrawerActivity {
     private ProgressDialog pDialog;
     private List<Notice> noticeList = new ArrayList<>();
     private ListView lv;
+    private ProgressBar progressBar;
     private NoticeListAdapter adapter;
     private DBHelper dbHelper;
 
     private FloatingActionButton fab_writing;
     private FloatingActionButton fab_search_property;
+
+    private int total_page;
+
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,18 +90,20 @@ public class NoticeActivity extends NavigationDrawerActivity {
 
         dbHelper = new DBHelper(this);
 
+        handler = new Handler();
+
+        final View footer = getLayoutInflater().inflate(R.layout.progress_bar_footer, null);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
         lv = (ListView) findViewById(R.id.lv);
         adapter = new NoticeListAdapter(this, noticeList);
         lv.setAdapter(adapter);
+        lv.addFooterView(footer);
 
         pDialog = new ProgressDialog(this);
         // Showing progress dialog before making http request
         pDialog.setMessage("Loading...");
         pDialog.show();
-
-        /*// changing action bar color
-        getSupportActionBar().setBackgroundDrawable(
-                new ColorDrawable(Color.parseColor("#1b1b1b")));*/
 
         // Creating volley request obj
         JsonArrayRequest noticeReq = new JsonArrayRequest(getString(R.string.certis_test_url),
@@ -106,22 +116,24 @@ public class NoticeActivity extends NavigationDrawerActivity {
                         // Parsing json
                         for (int i = 0; i < response.length(); i++) {
                             try {
-
                                 JSONObject obj = response.getJSONObject(i);
-                                Notice notice = new Notice();
-                                notice.setTitle(obj.getString("title"));
-                                notice.setThumbnailUrl(obj.getString("image"));
-                                notice.setUid(obj.getString("uid"));
-                                notice.setCreated_at(obj.getString("created_at"));
-                                notice.setId(obj.getInt("id"));
 
-                                // adding notice to notices array
-                                noticeList.add(notice);
+                                if (i == 0) {
+                                    total_page = obj.getInt("total_page");
+                                } else {
+                                    Notice notice = new Notice();
+                                    notice.setTitle(obj.getString("title"));
+                                    notice.setThumbnailUrl(obj.getString("image"));
+                                    notice.setUid(obj.getString("uid"));
+                                    notice.setCreated_at(obj.getString("created_at"));
+                                    notice.setId(obj.getInt("id"));
 
+                                    // adding notice to notices array
+                                    noticeList.add(notice);
+                                }
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
-
                         }
 
                         // notifying list adapter about data changes
@@ -146,6 +158,82 @@ public class NoticeActivity extends NavigationDrawerActivity {
 
         // Adding request to request queue
         AppController.getInstance().addToRequestQueue(noticeReq);
+
+        lv.setOnScrollListener(new EndlessScrollListener() {
+            @Override
+            public boolean onLoadMore(int page, int totalItemsCount) {
+                handler.postDelayed(loadMoreDataRequest(page), 3000);
+
+                if (page == total_page) {
+                    lv.removeFooterView(footer);
+                }
+                return true;
+            }
+        });
+    }
+
+    // Append more data into the adapter
+    public Runnable loadMoreDataRequest(final int offset) {
+        // This method probably sends out a network request and appends new data items to your adapter.
+        // Use the offset value and add it as a parameter to your API request to retrieve paginated data.
+        // Deserialize API response and then construct new objects to append to the adapter
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                // Creating volley request obj
+                JsonArrayRequest noticeReq = new JsonArrayRequest(getString(R.string.certis_test_add_request_url) + offset,
+                        new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                Log.d(TAG, response.toString());
+                                hidePDialog();
+
+                                // 첫 번째 요청(인덱스(0))은 페이지 관련 데이터를 응답 받아서 제외
+                                for (int i = 1; i < response.length(); i++) {
+                                    try {
+                                        JSONObject obj = response.getJSONObject(i);
+                                        Notice notice = new Notice();
+                                        notice.setTitle(obj.getString("title"));
+                                        notice.setThumbnailUrl(obj.getString("image"));
+                                        notice.setUid(obj.getString("uid"));
+                                        notice.setCreated_at(obj.getString("created_at"));
+                                        notice.setId(obj.getInt("id"));
+
+                                        // adding notice to notices array
+                                        noticeList.add(notice);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                }
+
+                                // notifying list adapter about data changes
+                                // so that it renders the list view with updated data
+                                adapter.notifyDataSetChanged();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        VolleyLog.d(TAG, "Error: " + error.getMessage());
+                        hidePDialog();
+                    }
+                }) {
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("Accept", "application/json");
+                        params.put("Authorization", "Token " + "token=" + dbHelper.dbSelect());
+                        return params;
+                    }
+                };
+
+                // Adding request to request queue
+                AppController.getInstance().addToRequestQueue(noticeReq);
+            }
+        };
+
+        return runnable;
     }
 
     @Override
